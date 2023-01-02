df51ed31a014e2c15b77c165c1fd280ba5455b47
Sarvottam <sarvottamswaroop100@gmail.com>
2023-01-02 23:33:45 +0530

    added quaternion slerp for smooth rotation

--- Assets/Script/Waypoints/FollowWP.cs
+++ Assets/Script/Waypoints/FollowWP.cs
@@ -8,6 +8,7 @@ public class FollowWP : MonoBehaviour
     int currentWP = 0;
 
     public float speed = 10.0f;
+    public float rotSpeed = 0.00001f;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +24,9 @@ public class FollowWP : MonoBehaviour
         if (currentWP >= waypoints.Length)
             currentWP = 0;
 
-        this.transform.LookAt(waypoints[currentWP].transform);
+        Quaternion lookWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookWP, rotSpeed * Time.deltaTime);
+        //this.transform.LookAt(waypoints[currentWP].transform);
         this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 }
