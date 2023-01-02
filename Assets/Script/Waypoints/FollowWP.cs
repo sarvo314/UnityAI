40b267fa8b9d21c71dc0369914ce47d3e522eb24
Sarvottam <sarvottamswaroop100@gmail.com>
2023-01-03 00:04:34 +0530

    Added a progress tracker so that tank doesn't goes off the track

--- Assets/Script/Waypoints/FollowWP.cs
+++ Assets/Script/Waypoints/FollowWP.cs
@@ -10,21 +10,40 @@ public class FollowWP : MonoBehaviour
     public float speed = 10.0f;
     public float rotSpeed = 0.00001f;
     // Start is called before the first frame update
+
+    GameObject tracker;
     void Start()
     {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+    void ProgressTracker()
+    {
+        NextWaypoint();
 
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, 10f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void NextWaypoint()
     {
-        if (Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 3)
             currentWP++;
 
         if (currentWP >= waypoints.Length)
             currentWP = 0;
+    }
 
-        Quaternion lookWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProgressTracker();
+
+        Quaternion lookWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
         this.transform.rotation = Quaternion.Slerp(this.transform.rotation, lookWP, rotSpeed * Time.deltaTime);
         //this.transform.LookAt(waypoints[currentWP].transform);
         this.transform.Translate(0, 0, speed * Time.deltaTime);
