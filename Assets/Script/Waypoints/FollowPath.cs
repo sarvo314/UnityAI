d9a575ddae772adc2058fecf383baa115837e5b7
Sarvottam <sarvottamswaroop100@gmail.com>
2023-01-09 23:02:40 +0530

    Added A* traversal

---
+++ Assets/Script/Waypoints/FollowPath.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPath : MonoBehaviour
+{
+
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 1.0f;
+    float rotSpeed = 2.0f;
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+    }
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[1]);
+        currentWP = 0;
+    }
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[6]);
+        currentWP = 0;
+
+    }
+    private void LateUpdate()
+    {
+        if (g.getPathLength() == 0 || currentWP == g.getPathLength())
+        { return; }
+
+        currentNode = g.getPathPoint(currentWP);
+
+        if (Vector3.Distance(currentNode.transform.position, transform.position) < accuracy)
+            currentWP++;
+
+        if (currentWP < g.getPathLength())
+        {
+            goal = g.getPathPoint(currentWP).transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y, goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+
+
+    }
+    void Update()
+    {
+
+    }
+}
